ea165e415d3c962088704527f6568a86e7a1a575
osakac <shinmoriof@gmail.com>
2025-02-11 00:43:58 +0400

    add: header

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
-<script setup lang="ts"></script>
-
 <template>
+  <TheHeader />
+
   <router-view></router-view>
 </template>
+
+<script setup lang="ts">
+import TheHeader from './components/Header/TheHeader.vue'
+</script>
